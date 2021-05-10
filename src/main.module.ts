2f82195cac790044c3e13ea37dae00c48135cd71
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-05-10 17:00:24 -0300

    build: instalar o módulo config

--- src/main.module.ts
+++ src/main.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ToolModule } from './tool/tool.module';
 
 @Module({
-  imports: [ToolModule],
+  imports: [ToolModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [],
   providers: [],
 })
