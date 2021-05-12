9b6f7f4c19ff94b6695ba32e1c1a5704f3e4e30d
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-05-12 18:23:53 -0300

    chore: habilitar o cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import { MainModule } from './main.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
+
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({ forbidUnknownValues: true, whitelist: true }),
   );
+
   await app.listen(3000);
 }
 bootstrap();
