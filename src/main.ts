f27857cf57e4f07c7b8f56c2a000dd873516f900
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-05-12 18:27:21 -0300

    build: adicionar o helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as helmet from 'helmet';
 
 import { MainModule } from './main.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
 
+  app.use(helmet());
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({ forbidUnknownValues: true, whitelist: true }),
