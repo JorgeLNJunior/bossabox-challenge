a3a9398c7a6e387805f8f9ec84bf1b48e1b456d4
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-05-20 15:29:07 -0300

    refactor: adicionar mais validações a tool query

--- src/modules/tool/query/toolQuery.ts
+++ src/modules/tool/query/toolQuery.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional } from 'class-validator';
+import { IsNumberString, IsOptional } from 'class-validator';
 
 export class ToolQuery {
   @ApiProperty({ required: false })
@@ -16,5 +16,6 @@ export class ToolQuery {
 
   @ApiProperty({ required: false })
   @IsOptional()
+  @IsNumberString()
   limit?: string;
 }
