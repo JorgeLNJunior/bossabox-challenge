698f669a0818d25fd1bb9d7967e1336ca946eaed
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-05-20 15:01:20 -0300

    feat: criar o módulo de usuário

---
+++ src/modules/user/dto/create-user.dto.ts
@@ -0,0 +1,23 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  password: string;
+}
