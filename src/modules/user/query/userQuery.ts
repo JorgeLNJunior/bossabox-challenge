dfd401cc37f25243068c3d8711f6615022799e1f
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-05-22 15:24:57 -0300

    docs: atualizar a documentação das rotas /user

--- src/modules/user/query/userQuery.ts
+++ src/modules/user/query/userQuery.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumberString, IsOptional, IsString } from 'class-validator';
 
 export class UserQuery {
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   _id?: string;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   name?: string;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsNumberString()
   limit?: string;
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiQuery,
+  ApiTags,
+  ApiTooManyRequestsResponse,
+} from '@nestjs/swagger';
 
 import { UserQuery } from './query/userQuery';
 import { UserService } from './user.service';
 
+@ApiBearerAuth()
+@ApiTags('User')
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiQuery({
+    type: UserQuery,
+  })
+  @ApiOkResponse({ description: 'return a list of users' })
+  @ApiTooManyRequestsResponse({ description: 'too many requests' })
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   async findAll(@Query() userQuery: UserQuery) {
     const users = await this.userService.findAll(userQuery);
